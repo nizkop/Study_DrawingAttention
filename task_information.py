b732a9edf37bc57b5ed3948835c2baf59c28e39b
Judith <judith.leson@stud.uni-due.de>
2026-04-10 18:10:43 +0200

    first idea, task data

---
+++ task_information.py
@@ -0,0 +1,9 @@
+from Participant import Participant
+from Task import Task
+
+
+def task_information(p:Participant, t:Task):
+    file_name = f"DATA/participant_{p.id}/task_{p.id}_{t.id}.json"
+
+
+    time_taken_in_sec = "Timestamp where participant realizes " - "Start (start of new slide) "# =T3-S3
